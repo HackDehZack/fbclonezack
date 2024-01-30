5792695a2c449a41c8fcf3c67df3aa911a3c0a0b
HackDehZack <Zackseriousemail@gmail.com>
2024-01-30 17:33:05 -0500

    checkpoint 1

---
+++ src/loginPage/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginPage from "./LoginPage.css";
+function UrbanUptakeloginpage() {
+    return (
+        <div className="login">
+            <div className="UrbanUptake">
+                <div className="UrbanUptaketext">
+                    UrbanUptake
+                </div>
+            </div>
+        </div>
+    );
+}
+export default LoginPage
