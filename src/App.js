bf55de36f6aad7a7c98bdf71b2ffc15db77d9738
HackDehZack <Zackseriousemail@gmail.com>
2024-01-30 20:00:25 -0500

    Fix formatting in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch} from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import LoginPage from './loginPage/LoginPage';
 import HeaderArea from './HeaderArea/HeaderArea';
 
 function App() {
   return (
     <div className="App">
-
-
       <Switch>
-        <Route path='/LoginPage' exact={true} component={LoginPage}/>
-        <Route path='/HeaderArea' exact={true} component={HeaderArea}/>
+        <Route path='/LoginPage' exact={true} component={LoginPage} />
+        <Route path='/HeaderArea' exact={true} component={HeaderArea} />
       </Switch>
     </div>
   );
 }
 
 export default App;
- 
